bec6477981b393505a6f187949cfb2723d46b7e4
erikalves12 <97927458+erikalves12@users.noreply.github.com>
2022-06-14 22:45:37 -0300

    aletr

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,12 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  
+  dialectOption: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = connection;
